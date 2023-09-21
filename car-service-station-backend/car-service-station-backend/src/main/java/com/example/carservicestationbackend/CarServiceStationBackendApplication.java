8ccc3e3425cb3c6f37487de332564b4adbb51cc5
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-09-21 13:44:26 +0200

    test

---
+++ car-service-station-backend/car-service-station-backend/src/main/java/com/example/carservicestationbackend/CarServiceStationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.carservicestationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceStationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceStationBackendApplication.class, args);
+	}
+
+}
---
+++ car-service-station-backend/car-service-station-backend/src/test/java/com/example/carservicestationbackend/CarServiceStationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carservicestationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceStationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
